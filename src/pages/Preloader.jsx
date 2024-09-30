48cbb6e65bb298118f3003013603b170ed288303
abhishek7088 <abhi7088721935@gmail.com>
2024-09-30 18:40:25 +0530

    preloader

---
+++ src/pages/Preloader.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import './Preloader.css';
+import { preLoaderAnim } from '../Animation/index.js';
+
+const Preloader = () => {
+    useEffect(() => {
+        preLoaderAnim();
+    }, []);
+
+    return (
+        <div id="loader" className="preloader" role="status" aria-live="polite">
+            <div className="texts-container text-gray-orange " >
+                <span >CREATION</span>
+                <span>INNOVATION</span>
+                <span>VIBE</span>
+            </div>
+        </div>
+    );
+};
+
+export default Preloader;
